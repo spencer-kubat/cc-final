db2b07b84ac9b0de633786a40201a5491e839466
Dakota Snyder <snyderdakota8@gmail.com>
2025-11-30 22:28:51 -0600

    Created updated waterscene added And added lighting/fog

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
-
+scene.add(new THREE.AmbientLight(0x88aaff, 0.6));
+scene.fog = new THREE.FogExp2(0x7db6ff,0.02); //adjust 2nd parameter for density of fog 
 // LOAD GLB
 const loader = new GLTFLoader();
 loader.load(
-    './CreativeCoding.glb',
+    './WaterEnvironment.glb',
     function (gltf) {
+    
         scene.add(gltf.scene);
         console.log("Model loaded", gltf);
     },
@@ -22,12 +24,13 @@ loader.load(
     }
 );
 
-// LIGHT
-const light = new THREE.PointLight(0x975111, 50, 1000);
-light.position.set(0, 3, 0);
+
+// AboveWater LIGHT
+const light = new THREE.DirectionalLight(0xffffff, 2);
+light.position.set(10, 20, 10);
 scene.add(light);
 
-// CAMERA
+// CAMERA 
 const width = window.innerWidth;
 const height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 100);
